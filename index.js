40b7c43c18649c29b2cc88b743d38f908c11f960
sumaiyakhatun11 <sumaiyameghla1111@gmail.com>
2025-12-07 22:20:31 +0600

    done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://BackendPractice:L0Aqe2ZQm0bxLRJh@cluster0.052zdja.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.052zdja.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const database = client.db('PetService');
         const serviceCollection = database.collection('services');
@@ -39,9 +39,15 @@ async function run() {
         })
 
         app.get('/services', async (req, res) => {
-            const result = await serviceCollection.find().toArray();
+            const { limit } = req.query; // e.g., ?limit=6
+            let cursor = serviceCollection.find().sort({ createdAt: -1 });
+
+            if (limit) cursor = cursor.limit(parseInt(limit));
+
+            const result = await cursor.toArray();
             res.send(result);
-        })
+        });
+
         app.get('/services/:id', async (req, res) => {
             const id = req.params;
             const query = { _id: new ObjectId(id) };
@@ -96,7 +102,7 @@ async function run() {
 
         });
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
